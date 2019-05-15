3474f5e8696200d72e258ab432d4a092e2bba694
melsem <aswerklon@gmail.com>
2019-05-15 21:25:11 +0300

    sunxi: edit wifi dts-uboot-patch
    packages xradio - OK
    
    Signed-off-by: Semen Kreyda <aswerklon@gmail.com>

--- target/linux/sunxi/modules.mk
+++ target/linux/sunxi/modules.mk
@@ -25,15 +25,54 @@ $(eval $(call KernelPackage,rtc-sunxi))
 define KernelPackage/sunxi-ir
     SUBMENU:=$(OTHER_MENU)
     TITLE:=Sunxi SoC built-in IR support (A20)
-    DEPENDS:=@TARGET_sunxi +kmod-input-core
+    DEPENDS:=@TARGET_sunxi +kmod-input-core +evtest
     $(call AddDepends/rtc)
     KCONFIG:= \
 	CONFIG_MEDIA_SUPPORT=y \
 	CONFIG_MEDIA_RC_SUPPORT=y \
 	CONFIG_RC_DEVICES=y \
-	CONFIG_IR_SUNXI
-    FILES:=$(LINUX_DIR)/drivers/media/rc/sunxi-cir.ko
-    AUTOLOAD:=$(call AutoLoad,50,sunxi-cir)
+	CONFIG_IR_SUNXI=y \
+	CONFIG_RC_DECODERS=y \
+	CONFIG_RC_CORE=m \
+	CONFIG_LIRC=m \
+	CONFIG_IR_NEC_DECODER=m \
+	CONFIG_IR_RC5_DECODER=m \
+	CONFIG_IR_RC6_DECODER=m \
+	CONFIG_IR_JVC_DECODER=m \
+	CONFIG_IR_SONY_DECODER=m \
+	CONFIG_IR_SANYO_DECODER=m \
+	CONFIG_IR_SHARP_DECODER=m \
+	CONFIG_IR_MCE_KBD_DECODER=m \
+	CONFIG_IR_LIRC_CODEC=y \
+	CONFIG_IR_XMP_DECODER=m \
+	CONFIG_IR_SPI=m \
+	CONFIG_IR_GPIO_TX=m \
+	CONFIG_IR_PWM_TX=m \
+	CONFIG_IR_SERIAL=m \
+	CONFIG_IR_SERIAL_TRANSMITTER=y \
+	CONFIG_IR_SIR=m \
+	CONFIG_IR_GPIO_CIR=m \
+	CONFIG_RC_MAP=m
+    FILES:= \
+	$(LINUX_DIR)/drivers/media/rc/sunxi-cir.ko \
+	$(LINUX_DIR)/drivers/media/rc/rc-core.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-lirc-codec.ko \
+	$(LINUX_DIR)/drivers/media/rc/lirc_dev.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-nec-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-rc5-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-rc6-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-jvc-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-sony-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-sanyo-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-sharp-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-mce_kbd-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-xmp-decoder.ko \
+	$(LINUX_DIR)/drivers/media/rc/serial_ir.ko \
+	$(LINUX_DIR)/drivers/media/rc/gpio-ir-tx.ko \
+	$(LINUX_DIR)/drivers/media/rc/ir-spi.ko \
+	$(LINUX_DIR)/drivers/media/rc/sir_ir.ko \
+	$(LINUX_DIR)/drivers/media/rc/pwm-ir-tx.ko
+    AUTOLOAD:=$(call AutoLoad,80,sunxi-cir)
 endef
 
 define KernelPackage/sunxi-ir/description
@@ -41,6 +80,7 @@ define KernelPackage/sunxi-ir/description
 endef
 
 $(eval $(call KernelPackage,sunxi-ir))
+
 
 define KernelPackage/ata-sunxi
     TITLE:=AllWinner sunXi AHCI SATA support
@@ -147,3 +187,60 @@ define KernelPackage/sound-soc-sun4i-spdif/description
 endef
 
 $(eval $(call KernelPackage,sound-soc-sun4i-spdif))
+
+#####################################################
+
+define KernelPackage/sun_graphics
+  SUBMENU:=Graphics Support Sunxi
+  TITLE:=AllWinner Graphics Support Sunxi
+  DEPENDS:=@TARGET_sunxi
+  KCONFIG:= \
+	CONFIG_DRM=m \
+	CONFIG_DRM_MIPI_DSI=y \
+	CONFIG_DRM_KMS_HELPER=y \
+	CONFIG_DRM_KMS_FB_HELPER=y \
+	CONFIG_DRM_FBDEV_EMULATION=y \
+	CONFIG_DRM_FBDEV_OVERALLOC=100 \
+	CONFIG_DRM_LOAD_EDID_FIRMWARE=y \
+	CONFIG_DRM_TTM=m \
+	CONFIG_DRM_GEM_CMA_HELPER=y \
+	CONFIG_DRM_KMS_CMA_HELPER=y \
+	CONFIG_DRM_SCHED=m \
+  FILES:= \
+	$(LINUX_DIR)/drivers/gpu/drm/drm_kms_helper.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/drm.ko
+  AUTOLOAD:=$(call AutoProbe,sun_graphics)
+endef
+
+$(eval $(call KernelPackage,sun_graphics))
+
+
+define KernelPackage/sun_amd_library
+  SUBMENU:=AMD Library routines
+  TITLE:=AllWinner AMD Library routines
+  DEPENDS:=@TARGET_sunxi
+  KCONFIG:= \
+	CONFIG_DRM_VKMS=m \
+	CONFIG_DRM_RCAR_DW_HDMI=m \
+	CONFIG_DRM_SUN4I=y \
+	CONFIG_DRM_SUN4I_HDMI=y \
+	CONFIG_DRM_SUN4I_HDMI_CEC=y \
+	CONFIG_DRM_SUN4I_BACKEND=y \
+	CONFIG_DRM_SUN6I_DSI=m \
+	CONFIG_DRM_SUN8I_DW_HDMI=y \
+	CONFIG_DRM_SUN8I_MIXER=y \
+	CONFIG_DRM_SUN8I_TCON_TOP=y \
+	CONFIG_DRM_PANEL=y
+  FILES:= \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun4i_tv.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun4i-drm.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun6i_drc.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun4i-tcon.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun8i-mixer.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun4i-backend.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/sun4i/sun4i-drm-hdmi.ko \
+	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-hdmi.ko
+  AUTOLOAD:=$(call AutoProbe,sun_amd_library)
+endef
+
+$(eval $(call KernelPackage,sun_amd_library))
